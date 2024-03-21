5d071eff989274d2490155d294ee09d3e3f4d998
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-03-20 23:15:05 -0300

    first commit

---
+++ src/Guesses/Program.cs
@@ -0,0 +1,7 @@
+using Guesses;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ src/Persistence/Program.cs
@@ -0,0 +1,7 @@
+using PalpiteFC.Worker.Persistence;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
