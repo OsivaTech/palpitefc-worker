1a59956b13f15cbe8bd31c26e1fbeaf4fb407702
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-05-20 14:36:53 -0300

    fix(DataContracts): fix GuessMessage properties names

--- src/DataContracts/MessageTypes/GuessMessage.cs
+++ src/DataContracts/MessageTypes/GuessMessage.cs
@@ -1,10 +1,11 @@
 ﻿namespace PalpiteFC.DataContracts.MessageTypes;
+
 public class GuessMessage
 {
     public int FixtureId { get; set; }
     public int UserId { get; set; }
-    public int HomeTeamId { get; set; }
-    public int HomeTeamGoals { get; set; }
-    public int AwayTeamId { get; set; }
-    public int AwayTeamGoals { get; set; }
+    public int HomeId { get; set; }
+    public int HomeGoals { get; set; }
+    public int AwayId { get; set; }
+    public int AwayGoals { get; set; }
 }
